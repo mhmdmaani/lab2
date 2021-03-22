256f1ed9d1fd52fff1822255b407bd270071f7b7
mhmdmaani <mohammedmaani@hotmail.com>
2021-03-22 23:31:31 +0100

    adding add method

---
+++ test/CalculatorTest.java
@@ -0,0 +1,42 @@
+import jdk.jfr.Description;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    Calculator calc = new Calculator();
+
+    @Test
+    @DisplayName("test add method with empty string")
+    void testAddMethodWithEmptySting(){
+        int numString = calc.add("");
+        assertEquals(numString,0);
+    }
+    @Test
+    @DisplayName("test add method with one number string")
+    void testAddMethodWithOneNumberSting(){
+        int numString = calc.add("50");
+        assertEquals(numString,50);
+    }
+    @Test
+    @DisplayName("test add method with two numbers")
+    void testAddMethodWithTwoNumbers(){
+        int numString = calc.add("5,6");
+        assertEquals(numString,11);
+    }
+
+    @Test
+    @DisplayName("test add method with two numbers containing spaces")
+    void testAddMethodWithTwoNumbersContainSpaces(){
+        int numString = calc.add("5 ,6 ");
+        assertEquals(numString,11);
+    }
+
+    @Test
+    @DisplayName("test add method with two numbers one of them is just space")
+    void testAddMethodWithOneEmptyNumber(){
+        int numString = calc.add("5 , ");
+        assertEquals(numString,5);
+    }
+}
